c274975a896ef669b24c67337475524d9fc1cd71
osama-mustafa <semsem6661@gmail.com>
2022-05-27 18:32:48 +0200

    Fix pop method in SSL

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -1,23 +1,22 @@
 class Node {
     constructor(val) {
-        this.val    = val;
-        this.next   = null;
+        this.val = val;
+        this.next = null;
     }
 }
 
 class SinglyLinkedList {
     constructor() {
-        this.head   = null;
-        this.tail   = null;
+        this.head = null;
+        this.tail = null;
         this.length = 0;
     }
 
     push(val) {
-
         let newNode = new Node(val);
         if (!this.head) {
             this.head = newNode;
-            this.tail = newNode
+            this.tail = newNode;
         } else {
             this.tail.next = newNode;
             this.tail = newNode;
@@ -40,26 +39,42 @@ class SinglyLinkedList {
     }
 
     shift() {
-
         // If list is empty
         if (!this.head) return undefined;
 
         // If list is not empty
-        let currentHead     = this.head;
-        this.head           = currentHead.next;
+        let currentHead = this.head;
+        this.head = currentHead.next;
         this.length--;
         if (this.length === 0) {
-            this.tail = null;
+        this.tail = null;
         }
         return currentHead;
     }
 
-
+    pop() {
+        if (!this.head) {
+            return undefined;
+        } else if (this.length == 1) {
+            let targetNode = this.head;
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return targetNode;
+        } else {
+            let current = this.head;
+            let targetNode = this.tail;
+            while (current.next.next != null) {
+                current = current.next;
+            }
+            this.tail = current;
+            this.tail.next = null;
+            this.length--;
+            return targetNode;
+        }
+    }
 }
 
-
-let singlyLinkedList = new SinglyLinkedList;
-console.log(singlyLinkedList.push(10).push(20).shift());
+let singlyLinkedList = new SinglyLinkedList();
+console.log(singlyLinkedList.push(10).push(20).push(30));
 console.log(singlyLinkedList);
-
-
